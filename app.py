2fb4a1e1ad4b227093bfb4485c650c12561869a6
GiriRanganathan <166815402+GiriRanganathan@users.noreply.github.com>
2024-05-20 10:09:25 +0000

    app.py updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,jsonify
+from flask import Flask,render_template,jsonify,request
 
 from src.pipeline.prediction_pipeline import PredictPipeline,CustomData
 
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return render_template("index.html")
+
+@app.route("/predict",methods=["GETS","POST"])
+def predict_datapoint():
+    if request.method=="GET":
+        return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8000)
